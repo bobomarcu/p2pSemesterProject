896f4e2f11d4636b2866730b8c1cce3fe739988d
Bogdan Marcu <bogdanmarcu2002@icloud.com>
2025-12-28 17:29:58 +0200

    FE: switch browsing manifest button for back to home to top postion

--- web-client/src/views/BrowseManifestsPage.js
+++ web-client/src/views/BrowseManifestsPage.js
@@ -4,8 +4,8 @@ import Button from "../components/Button"; // Import Button component
 export default function BrowseManifestsPage() {
     return (
         <section>
-            <h3>Browse Manifests</h3>
             <Button to="/">Back to Home</Button>
+            <h3>Browse Manifests</h3>
             {/* Content for browsing manifests will go here */}
         </section>
     );
